c3262e2aa5a48d8ef5dd34a2a65e194da80931af
Joseph <jstocke102@gmail.com>
2022-03-19 12:18:11 -0400

    Added ExtractingChuteBlock

--- src/main/java/com/jstocke/extractingchutes/ExtractingChutes.java
+++ src/main/java/com/jstocke/extractingchutes/ExtractingChutes.java
@@ -1,21 +1,15 @@
 package com.jstocke.extractingchutes;
 
-import net.minecraft.world.level.block.Block;
+import com.jstocke.extractingchutes.block.ModBlocks;
+import com.jstocke.extractingchutes.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExtractingChutes.MOD_ID)
@@ -26,8 +20,12 @@ public class ExtractingChutes
     private static final Logger LOGGER = LogManager.getLogger();
 
     public ExtractingChutes() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/com/jstocke/extractingchutes/block/ExtractingChuteBlock.java
@@ -0,0 +1,11 @@
+package com.jstocke.extractingchutes.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class ExtractingChuteBlock extends Block {
+    public ExtractingChuteBlock(Properties properties) {
+        super(properties);
+    }
+
+
+}
